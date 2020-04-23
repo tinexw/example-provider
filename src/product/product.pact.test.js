77474858e7a480059de3aa7f6ca194b54b5c993f
Kristine Jetzke <kristine.jetzke@kreuzwerker.de>
2020-04-23 13:39:47 +0200

    fix state name

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -38,7 +38,7 @@ describe("Pact Verification", () => {
     }
 
     const stateHandlers = {
-      "product with ID 10 exists": () => {
+      "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
           ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
         ]);
